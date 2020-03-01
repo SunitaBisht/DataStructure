ebfe1f8c89da068d2ae7779f56f69f6be46d1339
Sunita Bisht <sunnubisht2301@gmail.com>
2020-03-01 19:57:10 +0530

    project stucture added

---
+++ src/InsertionSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[100];
+            int i, j, n, temp=0;
+            Console.WriteLine("Enter the number of elements:");
+            n = int.Parse(Console.ReadLine());
+
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter the elements:");
+                a[i] = int.Parse(Console.ReadLine());
+
+            }
+            for (i = 0; i < n-1; i++)
+            {
+               j = i;
+               while (j > 0 && a[j] < a[j - 1])
+               {
+                    temp = a[j];
+                    a[j] = a[j - 1];
+                    a[j - 1] = temp;
+                    j--;
+                }
+            }
+            Console.WriteLine("The list in ascending order:\n");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}", a[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/SelectionSort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[100];
+            int i, j, temp = 0, n;
+            Console.WriteLine("Enter the number of elements:");
+            n = int.Parse(Console.ReadLine());
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter the elements:");
+                a[i] = int.Parse(Console.ReadLine());
+
+            }
+            for (i = 0; i < n; i++)
+            {
+                for (j = i + 1; j < n; j++)
+                {
+                    if (a[i] > a[j])            //for assending order when arrange in decending order so use less then(<)
+                    {
+                        temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
+                }
+            }
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}", a[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
